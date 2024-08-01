87ad1e2fc72aeaee2579534c35fe6add36d4a717
XFadel10X <assanifadel10@gmail.comm>
2024-08-01 21:01:48 +0200

    Reloading

--- scriptmsg.js
+++ scriptmsg.js
@@ -188,3 +188,11 @@ onAuthStateChanged(auth, user => {
         chatContainer.style.display = 'none';
     }
 });
+document.addEventListener('mousemove', function(e) {
+    const navbar = document.querySelector('.navbar');
+    if (e.clientX < 200) {
+        navbar.style.left = '0';
+    } else {
+        navbar.style.left = '-200px';
+    }
+});
